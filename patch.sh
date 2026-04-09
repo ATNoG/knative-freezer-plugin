302c7550863414c47c94a4c259ac67b6ceaf55b4
pmacoutinho <pmacoutinho@ua.pt>
2026-04-09 18:04:01 +0100

    add --restart flag to patch.sh for parity with unpatch.sh

--- patch.sh
+++ patch.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+# Patches the Knative queue-proxy to use the freezer plugin image.
+#
+# Usage:
+#   ./patch.sh                     # use "latest" tag
+#   ./patch.sh v1.2                # use a specific tag
+#   ./patch.sh --restart           # also restart all serving pods to pick it up
+#   ./patch.sh v1.2 --restart      # specific tag + restart
 set -e
 
 REGISTRY=ghcr.io/pmacoutinho
-TAG=${1:-latest}
+TAG="latest"
+RESTART=false
+
+for arg in "$@"; do
+    case "$arg" in
+        --restart) RESTART=true ;;
+        *)         TAG="$arg" ;;
+    esac
+done
 
 kubectl patch configmap config-deployment -n knative-serving \
   --type merge \
@@ -12,5 +27,12 @@ kubectl patch configmap config-features -n knative-serving \
   --type merge \
   -p '{"data":{"queueproxy.mount-podinfo":"enabled"}}'
 
-echo "Done. Restart existing serving pods to pick up the new queue-proxy:"
-echo "  kubectl rollout restart deployment -n <your-namespace>"
+echo "Done. Queue-proxy set to freezer plugin (tag: ${TAG})."
+
+if [[ "$RESTART" == "true" ]]; then
+    echo "Restarting all serving deployments in default namespace..."
+    kubectl rollout restart deployment -n default -l serving.knative.dev/service
+else
+    echo "Restart existing serving pods to pick up the change:"
+    echo "  kubectl rollout restart deployment -n <your-namespace>"
+fi
